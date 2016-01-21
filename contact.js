e3e2fafdae592c98c1f598bf80250fbc5d21e95a
SaanGithub <saanrashid@hotmail.com>
2016-01-21 11:12:58 +0100

    map icons slide up

--- contact.js
+++ contact.js
@@ -8,14 +8,18 @@ $(document).ready(function() {
 
 	$(".hideshowams").hover(function() {
 		$(".amsterdam").toggle();
+		$('#Container').css("background", "url(locations-amsterdam.jpg)");
 	});
 	$(".hideshowape").hover(function() {
 		$(".apeldoorn").toggle();
+		$('#Container').css("background", "url(locations-apeldoorn.jpg)");
 	});
 	$(".hideshowgoe").hover(function() {
 		$(".goes").toggle();
+		$('#Container').css("background", "url(locations-goes.jpg)");
 	});
 	$(".hideshowutr").hover(function() {
 		$(".utrecht").toggle();
+		$('#Container').css("background", "url(locations-utrecht.jpg)");
 	});
 });
